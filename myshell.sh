8885a185c235124c41ed338d6b6518bf0f87acc9
mtmar <moto.perin@gmail.com>
2021-06-27 10:12:18 +0900

    shellscriptの練習中です

--- myshell.sh
+++ myshell.sh
@@ -1,1 +1,31 @@
-echo "初めてのシェルスクリプト"
+# echo "初めてのシェルスクリプト"
+
+# read NAME
+# echo  "Hello $NAME!"
+
+# var="これは変数です"
+# VaR_2="これも変数です"
+# echo "VaR_2=$VaR_2"  
+
+#＄をつけると変数を呼び出せる
+
+# VaR_2="VaR_2が変更されました"
+# echo ${VaR_2}
+
+# readonly var
+# var="readonly varを変えてみる."
+
+# echo "\$0（スクリプト名）: $0"
+# echo "\$1（1番目の引数）: $1"
+# echo "\$2（2番目の引数）: $2"
+# echo "\$#（引数の数）: $#"
+# echo "\"\$*\": \"$*\""
+# echo "\"\$@\": \"$@\""
+# VAR="exit値は0になるはずです"
+# echo $?
+
+# echo "your scriptName: $0" >>スクリプト名を答えてくれます
+# echo "$$"
+
+
+
